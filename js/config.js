c6d3c082ca3550b0ed588d7894785c007802a9cc
katiheikkonen <kati.m.heikkonen@gmail.com>
2020-08-13 15:13:07 +0300

    Cognito ids added

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_u0gtzViMb',
+        userPoolClientId: '26lnt5d9htd9mb0tmc44k9516f', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
